4abaa4989ee4a21e72bdcbf4118e3386593912c6
mzeenkala23 <tachyonhd@gmail.com>
2020-04-21 22:08:05 +0100

    This is my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskSidebar from './components/Sidebar/Sidebar';
+
+
+function App() {
+  return (
+   <div>
+     <TaskSidebar/>
+   </div>
+  );
+}
+
+export default App;
